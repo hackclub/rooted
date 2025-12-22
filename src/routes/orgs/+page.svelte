627e30e5fe219cd19b06be473b549a3e357fd8d4
Freddie Yershon <143739931+hippogriff101@users.noreply.github.com>
2025-12-22 01:57:50 +0000

    Add Orgs page

---
+++ src/routes/orgs/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+    let bornhackOpen = $state(false);
+</script>
+
+<style>
+    @import "../styles.css";
+</style>
+
+<div class="about-container">
+    <div class="about-content">
+        <a href="/" class="back-btn">← back</a>
+
+        <div class="wip-banner">
+            <span class="icon">🚧</span>
+            <span class="text">Rooted is a work-in-progress.</span>
+        </div>
+        
+        <h1 class="title">Rooted Organisers</h1>
+
+        <div class="faq-item">
+            <h2 class="faq-question">Christian</h2>
+            <p class="faq-answer">
+                Christian is the Lead Organiser for Rooted.
+            </p>
+        </div>
+
+        <div class="faq-item">
+            <h2 class="faq-question">Mahad</h2>
+            <p class="faq-answer">
+                Mahad is one of the Co-Organiser for Rooted. He is a amazing developer and also works for Hack Club.
+            </p>
+        </div>
+
+        <div class="faq-item">
+            <h2 class="faq-question">Freddie</h2>
+            <p class="faq-answer">
+                Freddie is one of the Co-Organiser for Rooted. He loves coding and is excited to help out with Rooted
+            </p>
+        </div>
+
+        <div class="faq-item">
+            <h2 class="faq-question">Frog</h2>
+            <p class="faq-answer">
+                Frog is one of the Co-Organiser for Rooted. He is a great member of the team and is always ready to help.
+            </p>
+        </div>
+    </div>
+</div>
